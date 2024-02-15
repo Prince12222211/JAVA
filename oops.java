f65ac095c710e6100fa228b93fc38db8477aba29
Prince5047 <princeverma9504@gmail.com>
2024-02-16 00:03:45 +0530

    files are added

--- oops.java
+++ oops.java
@@ -91,30 +91,51 @@ import java.util.Scanner;
 
 
 //Copy Constructor
+// class Student{
+//     String name;
+//     int age;
+//     public void printInfo(){
+//         System.out.println(this.name);
+//         System.out.println(this.age);
+//     }
+//     Student(Student s2){
+//         this.name=s2.name;
+//         this.age=s2.age;
+//     }
+//     Student(){
+
+//     }
+
+// }
+// public class oops {
+//     public static void main(String args[]){
+//         Student s1=new Student();
+//         s1.name="Naruto";
+//         s1.age=16;
+//         Student s2=new Student(s1);
+//         s2.printInfo();
+//     }
+
+    
+// }
+
+
+//polymoriphism
 class Student{
     String name;
     int age;
-    public void printInfo(){
-        System.out.println(this.name);
-        System.out.println(this.age);
+    public void printInfo(String name){
+        System.out.println(name);
     }
-    Student(Student s2){
-        this.name=s2.name;
-        this.age=s2.age;
+    public void printInfo(int age){
+        System.out.println(age);
     }
-    Student(){
-
+    public void printInfo(String name,int age){
+        System.out.println(name+" "+age);
     }
 
 }
 public class oops {
-    public static void main(String args[]){
-        Student s1=new Student();
-        s1.name="Naruto";
-        s1.age=16;
-        Student s2=new Student(s1);
-        s2.printInfo();
-    }
 
     
 }

a63bf9194c3603ee192e4b1f31a79a33bda82e77
Prince5047 <princeverma9504@gmail.com>
2024-02-16 02:03:22 +0530

    files are added in the class files

--- oops.java
+++ oops.java
@@ -1,6 +1,8 @@
 import java.sql.Struct;
 import java.util.Scanner;
 
+// import Triangle.EquilTri;
+
 // class Pen {
 //     String color;
 //     String type;
@@ -89,7 +91,6 @@ import java.util.Scanner;
 //     }
 // }
 
-
 //Copy Constructor
 // class Student{
 //     String name;
@@ -116,26 +117,159 @@ import java.util.Scanner;
 //         s2.printInfo();
 //     }
 
-    
 // }
 
-
 //polymoriphism
-class Student{
-    String name;
-    int age;
-    public void printInfo(String name){
-        System.out.println(name);
-    }
-    public void printInfo(int age){
-        System.out.println(age);
-    }
-    public void printInfo(String name,int age){
-        System.out.println(name+" "+age);
-    }
+// class Student{
+//     String name;
+//     int age;
+//     public void printInfo(String name){
+//         System.out.println(name);
+//     }
+//     public void printInfo(int age){
+//         System.out.println(age);
+//     }
+//     public void printInfo(String name,int age){
+//         // System.out.println(name+" "+"ln"+age);
+//         System.out.println(name+'\n'+age);
+//     }
 
+// }
+// public class oops {
+// public static void main(String args[]){
+//     Student s1=new Student();
+//     s1.name="Prince";
+//     s1.age=20;
+//     s1.printInfo(s1.name,s1.age);
+//     // s1.printInfo("aman");
+//     // s1.printInfo(20);
+//     // s1.printInfo("Naruto", 16);
+// }
+
+// }
+
+//Inheritance
+// class Shape {
+//     public void area() {
+//         System.out.println("Displays area");
+//     }
+// }
+
+// class Triangle extends Shape {
+//     public void area(int l, int h) {
+//         System.out.println((1.0 / 2) * h * l);
+
+//     }
+// }
+
+// class EquilTri extends Triangle {
+//     public void area(int l, int h) {
+//         System.out.println(1.0 / 2 * h * l);
+//     }
+// }
+
+// class circle extends Shape {
+//     public void area(int r) {
+//         System.out.println((3.14) * r * r);
+//     }
+
+// }
+
+// /**
+//  * oops
+//  */
+// public class oops {
+
+//     public static void main(String args[]) {
+//         Triangle t1 = new Triangle();
+//         t1.area(45, 5);
+//         // equilateraltrinagle t2=new equilateraltrinagle();
+//         // EquilTri t2=new EquilTri();
+//         circle c1=new circle();
+//         c1.area(4);
+//         EquilTri t2=new EquilTri();
+//         t2.area(4, 8);
+//     }
+// }
+
+//Access modifier and packages
+// class Account {
+//     public String name;
+//     protected String email;
+//     private String password;
+
+//     // getter and setter =>Its used to access the private parts from the class
+
+//     public String getPassword() {
+//         return this.password;
+//     }
+
+//     public String setPassword(String pass) {
+//         return this.password=pass;
+//     }
+
+// }
+
+// public class oops {
+//     public static void main(String args[]) {
+//         Account account1 = new Account();
+//         account1.name = "Naruto";
+//         account1.email = "Naruto@gmail.com";
+//         account1.setPassword("prince123");
+//         System.out.println(account1.getPassword());
+
+//     }
+
+// }
+
+//Abstraction
+//  abstract class Animal {
+//     abstract void walk();
+// }
+
+// class Horse extends Animal{
+//     public void walk(){
+//         System.out.println("Walks on the 4 legs");
+//     }
+// }
+// class Chicken extends Animal{
+//     public void walk(){
+//         System.out.println("Walks on the 2 legs");
+//     }
+// }
+// public class oops {
+//     public static void main(String args[]) {
+//         Horse horse=new Horse();
+//         horse.walk();
+
+
+//     }
+
+// }
+
+//Interface
+interface Animal{
+    void walk();
+}
+interface Herbivore{
+
+}
+//multiple inhertance
+class Horse implements Animal,Herbivore{ //we can access the interface by implements
+public void walk(){
+    System.out.println("walks on the 4 legs");
+}
 }
 public class oops {
 
-    
+    public static void main(String args[]){
+        Horse horse =new Horse();
+        horse.walk();
+
+    }
 }
+
+
+/*Static
+ =>
+ */
